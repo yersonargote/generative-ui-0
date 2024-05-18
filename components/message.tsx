657573dedb16a7a1bd8eb2cbbddb732df124e4a9
yersonargote <yersonargotev@unicauca.edu.co>
2024-05-18 00:56:18 -0500

    feat: Add react-syntax-highlighter dependency for code highlighting

--- components/message.tsx
+++ components/message.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { StreamableValue, useStreamableValue } from 'ai/rsc'
-import { MemoizedReactMarkdown } from './ui/markdown'
+import 'katex/dist/katex.min.css'
+import React from 'react'
 import rehypeExternalLinks from 'rehype-external-links'
+import rehypeKatex from 'rehype-katex'
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
-import rehypeKatex from 'rehype-katex'
-import 'katex/dist/katex.min.css'
+import { CodeBlock } from './ui/codeblock'
+import { MemoizedReactMarkdown } from './ui/markdown'
 
 export function BotMessage({ content }: { content: StreamableValue<string> }) {
   const [data, error, pending] = useStreamableValue(content)
@@ -19,9 +21,54 @@ export function BotMessage({ content }: { content: StreamableValue<string> }) {
 
   return (
     <MemoizedReactMarkdown
+      className="prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 min-w-full space-y-6 break-words"
       rehypePlugins={[[rehypeExternalLinks, { target: '_blank' }], rehypeKatex]}
       remarkPlugins={[remarkGfm, remarkMath]}
-      className="prose-sm prose-neutral prose-a:text-accent-foreground/50"
+      components={{
+        p({ children }) {
+          return <p className="mb-2 last:mb-0">{children}</p>
+        },
+        img({ node, ...props }) {
+          return <img className="max-w-[67%]" {...props} />
+        },
+        code({ node, className, children, ...props }) {
+          const childArray = React.Children.toArray(children)
+          const firstChild = childArray[0] as React.ReactElement
+          const firstChildAsString = React.isValidElement(firstChild)
+            ? (firstChild as React.ReactElement).props.children
+            : firstChild
+
+          if (firstChildAsString === '▍') {
+            return <span className="mt-1 animate-pulse cursor-default">▍</span>
+          }
+
+          if (typeof firstChildAsString === 'string') {
+            childArray[0] = firstChildAsString.replace('`▍`', '▍')
+          }
+
+          const match = /language-(\w+)/.exec(className || '')
+
+          if (
+            typeof firstChildAsString === 'string' &&
+            !firstChildAsString.includes('\n')
+          ) {
+            return (
+              <code className={className} {...props}>
+                {childArray}
+              </code>
+            )
+          }
+
+          return (
+            <CodeBlock
+              key={Math.random()}
+              language={(match && match[1]) || ''}
+              value={String(childArray).replace(/\n$/, '')}
+              {...props}
+            />
+          )
+        }
+      }}
     >
       {processedData}
     </MemoizedReactMarkdown>
