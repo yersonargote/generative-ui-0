3b6b8709e60f45ba81efce4867463e4346983864
yersonargote <yersonargotev@unicauca.edu.co>
2024-05-19 17:45:57 -0500

    chore: Refactor search section component and update section titles to Spanish translations

--- components/search-section.tsx
+++ components/search-section.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { SearchResults } from './search-results'
-import { SearchSkeleton } from './search-skeleton'
-import { SearchResultsImageSection } from './search-results-image'
-import { Section } from './section'
-import { ToolBadge } from './tool-badge'
 import type { SearchResults as TypeSearchResults } from '@/lib/types'
 import { StreamableValue, useStreamableValue } from 'ai/rsc'
+import { SearchResults } from './search-results'
+import { SearchResultsImageSection } from './search-results-image'
+import { SearchSkeleton } from './search-skeleton'
+import { Section } from './section'
+import { ToolBadge } from './tool-badge'
 
 export type SearchSectionProps = {
   result?: StreamableValue<string>
@@ -23,14 +23,14 @@ export function SearchSection({ result }: SearchSectionProps) {
             <ToolBadge tool="search">{`${searchResults.query}`}</ToolBadge>
           </Section>
           {searchResults.images && searchResults.images.length > 0 && (
-            <Section title="Images">
+            <Section title="Imagenes">
               <SearchResultsImageSection
                 images={searchResults.images}
                 query={searchResults.query}
               />
             </Section>
           )}
-          <Section title="Results">
+          <Section title="Resultados">
             <SearchResults results={searchResults.results} />
           </Section>
         </>
